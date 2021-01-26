500e98698ab3d14d6c874da4ea4e002616dbc538
Taylor <ivingstaylor@gmail.com>
2021-01-26 18:23:12 +0000

    add back the red class in .addClass() because I apparently deleted it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#tweet-text").keyup(function() {
     $(".counter").text(140 - this.value.length);
     if (this.value.length > 140) {
-      $(".counter").addClass()
+      $(".counter").addClass("red");
     } else {
         $(".counter").removeClass("red");
     }
