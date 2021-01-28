7ddc50f0d1b9e772be255c909910cf5a79dbf5cc
Taylor <ivingstaylor@gmail.com>
2021-01-28 05:52:57 +0000

    update validation errors with jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,7 +38,7 @@ const createTweetElement = function(tweet) {
     <div class="name"><img src='${tweet.user.avatars}'> ${tweet.user.name}</div>
     <div class="handle">${tweet.user.handle}</div>
   </header>
-  <div>${tweet.content.text}</div>
+  <div>${escape(tweet.content.text)}</div>
   <div class="border"></div>
   <footer>
     <div>${moment(timeTweeted).startOf('minute').fromNow()}</div>
@@ -48,27 +48,46 @@ const createTweetElement = function(tweet) {
 
 return $tweet;
 }
-
 $(function() {
   const $button = $('#tweetbutt');
   $button.on('click', function(event) {
     event.preventDefault();
+    if ($('textarea').val().length > 140) {
+      $('#char').removeClass('invis').slideDown(200);
+    } else if ($('textarea').val() === null || $('textarea').val() === "") {
+      $('#null').removeClass('invis').slideDown(200);
+    } else if(decodeURI($('textarea').serialize()).trim() === 'text=') {
+      $('#space').removeClass('invis').slideDown(200);
+    } else {
       const data = $('textarea').serialize();
-      $.post('/tweets', data) 
+      console.log(decodeURI(data).trim());
+      $.post('/tweets', data)       
+      .then(function() {
+        loadTweets();
+        $('textarea').val('')
+        $('.counter').val(140);
+      })
     }
   })
 })
 
-
-
 loadTweets = () => {
-  $.getJSON('/tweets', data)
-    return data;
+  $.getJSON('/tweets')
+  .then(function (data) {
+    renderTweets(data);
+    $('#char').slideUp(0).addClass('invis');
+    $('#null').slideUp(0).addClass('invis');
+    $('#space').slideUp(0).addClass('invis');
+  })
 }
-const arr = loadTweets();
 
-renderTweets(arr)
+//
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
-
+loadTweets();
 });
 
